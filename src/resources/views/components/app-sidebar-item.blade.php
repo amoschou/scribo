e4762c611741a2c0e5a4fa7cff4335aa0f75ac41
amoschou <4948673+amoschou@users.noreply.github.com>
2024-03-17 11:17:50 +1030

    Updates

--- src/resources/views/components/app-sidebar-item.blade.php
+++ src/resources/views/components/app-sidebar-item.blade.php
@@ -4,13 +4,17 @@
 @endphp
 
 @foreach ($tree as $key => $treeItem)
+    @php
+        $tag = null; // $treeItem->object->findPartwiseOrder($rootTree);
+    @endphp
+
     @if ($loop->first)
         <ul class="{{ $root ? 'list-unstyled' : '' }} mb-0">
     @endif
 
     @if ($treeItem->object->type === 'file')
         <li class="list-unstyled">
-            <x-scribo::app-partwise-tag :tag="$treeItem->object->findPartwiseOrder($rootTree)" />
+            <x-scribo::app-partwise-tag :tag="$tag" />
            <a href="{{ $treeItem->object->href() }}">{!! $treeItem->object->metadata('formattedTitle') !!}</a>
         </li>
     @endif
@@ -18,7 +22,7 @@
     @if ($treeItem->object->type === 'folder')
         <details>
             <summary>
-                <x-scribo::app-partwise-tag :tag="$treeItem->object->findPartwiseOrder()" />{!! $treeItem->object->metadata('formattedTitle') !!}
+                <x-scribo::app-partwise-tag :tag="$tag" />{!! $treeItem->object->metadata('formattedTitle') !!}
             </summary>
 
             <x-scribo::app-sidebar-item :tree="$treeItem->tree" :root="false" :rootTree="$rootTree" />
